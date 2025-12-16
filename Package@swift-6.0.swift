a45b8133c3c1f072fa51f0480ece0f21d8a00abc
Adam Fowler <adamfowler71@gmail.com>
2025-12-16 11:46:20 +0000

    Add traits, and 6.0 package.swift

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,6 @@ let package = Package(
             targets: ["TraitsTest"]
         )
     ],
-    traits: [.trait(name: "TestTrait"), .default(enabledTraits: ["TestTrait"])],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
